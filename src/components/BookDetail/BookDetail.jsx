2037b873e0c27e9ecf9f9cee835a6e3a5a62183b
habibapanna <habibapanna49@gmail.com>
2024-11-02 21:35:25 +0600

    added components

--- src/components/BookDetail/BookDetail.jsx
+++ src/components/BookDetail/BookDetail.jsx
@@ -1,4 +1,5 @@
 import { useLoaderData, useParams } from "react-router-dom";
+import { addToStoredReadList } from "../../utility/addToDb";
 
 
 const BookDetail = () => {
@@ -14,13 +15,27 @@ const book = data.find(book => book.bookId === id)
 
 const {bookId: currentBookId, image} = book;
 
+const handleMarkAsRead = (id) => {
+/**
+ * 1. understand what to store or save: => bookId
+ * 2. Where to store: database
+ * 3. array, list, collection:
+ * 4. check: if the book is already in the read list
+ * 5. if not, then add the book to list
+ * 6. if yes, do not add the book
+ * **/ 
+
+addToStoredReadList(id);
+
+}
+
     return (
-        <div>
+        <div className="my-12">
             <h2>Book details: {bookId}</h2>
             <img className="w-36" src={image} alt="" />
             <br />
-            <button className="btn btn-outline mr-4 btn-accent">Read</button>
-            <button className="btn btn-accent">Wish List</button>
+            <button onClick={() => handleMarkAsRead(bookId)} className="btn btn-outline mr-4 btn-accent">Mark as Read</button>
+            <button className="btn btn-accent">Add to Wish List</button>
         </div>
     );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -11,6 +11,9 @@ import ErrorPage from './components/ErrorPage/ErrorPage';
 import Home from './components/Home/Home';
 import Dashboard from './components/Dashboard/Dashboard';
 import BookDetail from './components/BookDetail/BookDetail';
+import ListedBooks from './components/ListedBooks/ListedBooks';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const router = createBrowserRouter([
   {
@@ -25,7 +28,13 @@ const router = createBrowserRouter([
     {
       path: 'books/:bookId',
       element: <BookDetail></BookDetail>,
-      loader: () => fetch('/booksData.json')
+      loader: () => fetch('/booksData.json') // do not load all the books for one book.
+    },
+    {
+      path: 'listedBooks',
+      element: <ListedBooks></ListedBooks>,
+      // worst way to load some data.
+      loader: () => fetch('/booksData.json') // do not load all data for some
     },
     {
       path: 'dashboard',
@@ -38,5 +47,6 @@ const router = createBrowserRouter([
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <RouterProvider router={router} />
+    <ToastContainer />
   </StrictMode>,
 )
